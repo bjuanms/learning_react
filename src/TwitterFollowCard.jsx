92dd6405757d0fc8e2ab7c18808fa365aefd936f
bjuanms <rubialesms@gmail.com>
2023-06-15 14:42:40 +0200

    comittt

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react" //use state es un ARRAY
 
-export function TwitterFollowCard({ userName, name}) {//la base de la reutilización de los componentes son los PARÁMETROS
+export function TwitterFollowCard({ userName, name, initialIsFollowing}) {//la base de la reutilización de los componentes son los PARÁMETROS
     // export function TwitterFollowCard({ formatUserName, userName, name, isFollowing }) {//aqui mostramos la exportación del format username
     //<span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
-    const [isFollowing, setIsFollowing] = useState(false) //primera posición tiene el valor y el segundo la forma de actualizar
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing) //primera posición tiene el valor y el segundo la forma de actualizar
     // const isFollowing = state[0]
     // const setIsFollowing = state[1]
 
-    console.log(isFollowing)
+    console.log('twitter follow card - render whith username', userName)
     const text = isFollowing ? 'Siguiendo' : 'Seguir' //esto es la estructura de un if pero reducido, lo mismo es if(isFollowing){text = 'siguiendo}else{text='SEguir'}
     const buttonClassName = isFollowing  //aquí tenemos otra función. Si está siguiendo tiene un estilo y si no, tiene el estilo normal.
     ? 'tw-followCard-button is-following'
@@ -24,7 +24,7 @@ export function TwitterFollowCard({ userName, name}) {//la base de la reutilizac
                 <img
                     className='tw-followCard-avatar'
                     alt="Avatar" //El valor del atributo "alt" se muestra si la imagen no se puede cargar correctamente o si el usuario está utilizando un lector de pantalla.
-                    src={`https://unavatar.io/${userName}`}>
+                    src={`https://unavatar.io/${userName}`}> 
                 </img>
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
@@ -34,7 +34,8 @@ export function TwitterFollowCard({ userName, name}) {//la base de la reutilizac
 
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
--- src/app.jsx
+++ src/app.jsx
@@ -2,45 +2,101 @@ import { useState } from "react" //use state es un ARRAY
 import './App.css' //importamos los estilos de los elementos
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'Emma',
+        name: 'Francisco Laso',
+        isFollowing: true 
+    },
+    {
+        userName: 'bjuanms',
+        name: 'Peter Parker',
+        isFollowing: false
+    },
+    {
+        userName: 'Paris',
+        name: 'Arnold Heik',
+        isFollowing: true 
+    },
+]
+
+//-----------------ESTA ES LA VERSION 1 QUE NO SE USA PERO ESTÁ BIEN PARA ENTENDER------------------
+// export function App() {
+
+//     const [name, setName] = useState('alfred')
+
+//     console.log('render with name', name)
+//     // console.log(isFollowing)
+//     // const formatUserName = (userName) => `@${userName}`
+//     const primaco = { userName: 'primaco22', name: 'Jesus Alberto' }
+//     return (
+//         // esto <> </> es lo mismo que poner react.fragment pero así se evita tanto texto. Si no introducimos los elementos en un-
+//         //- fragment no se podrían repetir, daría error. Nosotros usamos section porque tendrá un estilo personalizado
+//         <section className='users_colum'>
+//             <TwitterFollowCard
+//                 // formatUserName={formatUserName}
+//                 userName={"rob"}
+//                 name={"George Pumpling"}
+//                 initialIsFollowing={true}
+//                 // isFollowing  is following ya no hace falta para nada, pues es un componente interno de 
+//             />
+//             <TwitterFollowCard
+//                 // formatUserName={formatUserName}
+//                 userName={name} // para lo de cambiar el nombre. El primer valor es alfred. al pulsar el botn cambiará
+//                 name={"Alfredo Carrascosa"}
+//                 initialIsFollowing={false}
+//             />
+//             <TwitterFollowCard
+//                 // formatUserName={formatUserName}
+//                 userName={"jhon"}
+//                 name={"Juan Martínez"}
+//                 initialIsFollowing={true}isFollowing
+
+//             />
+
+//             <TwitterFollowCard {...primaco}
+//             // también se puede hacer de esta forma. Creas una constante con los datos como json y con 3 puntos los pasas.
+//             />
+
+//             <button onClick={() => setName('Hijodeputa')}>
+//                 Cambio de Nombre
+//                  </button>
+
+
+//         </section>
+//     )
+// }
+
 export function App() {
 
-    const [name, setName] = useState('alfred')
-
-    console.log('render with name', name)
-    // console.log(isFollowing)
-    // const formatUserName = (userName) => `@${userName}`
-    const primaco = { userName: 'primaco22', name: 'Jesus Alberto' }
     return (
         // esto <> </> es lo mismo que poner react.fragment pero así se evita tanto texto. Si no introducimos los elementos en un-
         //- fragment no se podrían repetir, daría error. Nosotros usamos section porque tendrá un estilo personalizado
-        <section className='users_colum'>
-            <TwitterFollowCard
-                // formatUserName={formatUserName}
-                userName={"rob"}
-                name={"George Pumpling"}
-                // isFollowing  is following ya no hace falta para nada, pues es un componente interno de 
-            />
-            <TwitterFollowCard
-                // formatUserName={formatUserName}
-                userName={name} // para lo de cambiar el nombre. El primer valor es alfred. al pulsar el botn cambiará
-                name={"Alfredo Carrascosa"}
-                isFollowing={false}
-            />
-            <TwitterFollowCard
-                // formatUserName={formatUserName}
-                userName={"jhon"}
-                name={"Juan Martínez"}
-                isFollowing
+        <section className='App'>
+           {
+            users.map(user => {
+                const {userName, name, isFollowing } = user
+{/*Map es una función que te permite transformar los elementos de una lista y que devuelve una nueva lista con los elementos transformados.*/}
 
-            />
+                return (
+                   <TwitterFollowCard 
+                   key={userName} // la key debe ser algo que exista único y no se repita nunca
+                   userName={userName}
+                   initialIsFollowing={isFollowing}
+                   name={name}
+                   >
+                    
+                   </TwitterFollowCard>
+                )
 
-            <TwitterFollowCard {...primaco}
-            // también se puede hacer de esta forma. Creas una constante con los datos como json y con 3 puntos los pasas.
-            />
-
-            <button onClick={() => setName('Hijodeputa')}>
+            })
+           }
+            
+            
+            
+            {/* <button onClick={() => setName('Hijodeputa')}>
                 Cambio de Nombre
-                 </button>
+                 </button> */}
 
 
         </section>
